16d4b7ec1b069613f2e4812b2b187b42ee412e49
RafM <rkjmosiolek@gmail.com>
2018-12-08 17:39:24 +0000

    let app.js recognise user controller and map route to /users

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+const UserController = require('./user/UserController');
+app.use('/users', UserController);
+
 module.exports = app;
